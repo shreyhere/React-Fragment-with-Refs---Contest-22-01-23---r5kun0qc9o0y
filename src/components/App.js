d5dc3556cff8b66f0a34e84ff9bafc0ee3b5f379
shreyhere <116891326+shreyhere@users.noreply.github.com>
2023-01-22 21:45:07 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,37 @@
-import React from 'react';
-import ListItems from './ListItems';
+import React, { useState, useEffect, Fragment, useRef } from "react";
+import '../styles/App.css';
 
-const List = ({ listx }) => {
+import List from "./List";
+
+const App = () => {
+
+  const [value, setValue] = useState('');
+  const [list, setList] = useState([]);
+  const inputRef = useRef();
+
+  const onButtonClick = () => {
+
+    setList([]);
+    for (let i = 1; i < Number(value) + 1; i++) {
+      setList((prev) => [...prev, i]);
+    }
+    setValue('')
+    inputRef.current.focus();
+  };
+
+
+
+
   return (
-    // <div>
-      <>
-        {listx.map((i) => {
-          return <ListItems valuex={i} />;
-        })}
-      </>
-    // </div>
-  )
-}
+    <div id="main">
 
-export default List;
+      <input id="input" onChange={(e) => setValue(e.target.value)} value={value} ref={inputRef} />
+      <button id="button" onClick={onButtonClick}>Click</button>
+      <ul id="list">
+        <List listx={list} />
+      </ul>
+    </div>
+  );
+};
+
+export default App;
